cf0a97f3fe5a6bcebcf96ebaf9d8966855638492
crislmfroes <cris.lima.froes@gmail.com>
2018-12-15 21:56:58 -0200

    tentando fazer funcionar ...

--- simple-convolutional-network/main.py
+++ simple-convolutional-network/main.py
@@ -1,21 +1,58 @@
 import tensorflow as tf
-from tensorflow.python.keras import layers
+#from tensorflow.python.keras import layers
 
-model = tf.keras.Sequential([
-    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3), padding='same'),
-    layers.MaxPooling2D(pool_size=2),
+from tensorflow.python.keras.layers import *
+
+
+'''model = tf.keras.Sequential([
+    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)),
+    layers.Conv2D(32, (3, 3), activation='relu'),
+    layers.MaxPooling2D(pool_size=(2, 2)),
+    layers.Dropout(0.25),
     layers.Conv2D(64, (3, 3), activation='relu'),
-    layers.MaxPooling2D(pool_size=2),
     layers.Conv2D(64, (3, 3), activation='relu'),
-    layers.MaxPooling2D(pool_size=2),
+    layers.MaxPooling2D(pool_size=(2, 2)),
+    layers.Dropout(0.25),
     layers.Flatten(),
     layers.Dense(256, activation='relu'),
     layers.Dense(10, activation='softmax')
-])
+])'''
 
-model.compile(optimizer=tf.train.AdamOptimizer(0.001), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
+
+
+num_classes = 10
 (trainX, trainY), (testX, testY) = tf.keras.datasets.cifar10.load_data()
+
+
+model = tf.keras.models.Sequential()
+model.add(Conv2D(32, (3, 3), padding='same',
+                 input_shape=trainX.shape[1:]))
+model.add(Activation('relu'))
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(64, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes))
+model.add(Activation('softmax'))
+
+
+model.compile(optimizer=tf.train.AdamOptimizer(0.001), loss='categorical_crossentropy', metrics=['accuracy'])
+
+trainY = tf.keras.utils.to_categorical(trainY, num_classes=num_classes)
+testY = tf.keras.utils.to_categorical(testY, num_classes=num_classes)
 
 model.fit(trainX, trainY, batch_size=32, epochs=10)
 
