43ac784d560966a60e68cd61565714c9a7318ab5
crislmfroes <cris.lima.froes@gmail.com>
2018-12-15 21:05:18 -0200

    alterações na arquitetura da rede e na função de perda

--- simple-convolutional-network/main.py
+++ simple-convolutional-network/main.py
@@ -1,20 +1,24 @@
 import tensorflow as tf
 from tensorflow.python.keras import layers
 
-model = tf.keras.Sequential(
-    layers.Conv2D(32, 5, activation='relu'),
-    layers.MaxPool2D(),
-    layers.Conv2D(32, 5, activation='relu'),
-    layers.MaxPool2D(32, 5, activation='relu'),
-    layers.Conv2D(32, 5, activation='relu'),
-    layers.MaxPool2D(32, 5, activation='relu'),
-    layers.Dense(10, activation='relu')
-)
+model = tf.keras.Sequential([
+    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3), padding='same'),
+    layers.MaxPooling2D(pool_size=2),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D(pool_size=2),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D(pool_size=2),
+    layers.Flatten(),
+    layers.Dense(256, activation='relu'),
+    layers.Dense(10, activation='softmax')
+])
 
-model.compile(optimizer=tf.train.AdamOptimizer(0.001), loss='categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer=tf.train.AdamOptimizer(0.001), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-dataset = tf.keras.datasets.cifar10.load_data('../data/cifar10/')
+(trainX, trainY), (testX, testY) = tf.keras.datasets.cifar10.load_data()
 
-model.fit(dataset, epochs=10, steps_per_epoch=30)
+model.fit(trainX, trainY, batch_size=32, epochs=10)
 
 model.save('../models/cifar10-convolutional.pb')
+
+model.eval(testX, testY, batch_size=32)
