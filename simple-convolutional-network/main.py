bac7f5f1a4cf6e412716d86fe28da4a0d602f111
crislmfroes <cris.lima.froes@gmail.com>
2018-12-15 22:02:57 -0200

    tentando fazer funcionar ...

--- simple-convolutional-network/main.py
+++ simple-convolutional-network/main.py
@@ -1,10 +1,7 @@
 import tensorflow as tf
-#from tensorflow.python.keras import layers
+from tensorflow.python.keras import layers
 
-from tensorflow.python.keras.layers import *
-
-
-'''model = tf.keras.Sequential([
+model = tf.keras.Sequential([
     layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)),
     layers.Conv2D(32, (3, 3), activation='relu'),
     layers.MaxPooling2D(pool_size=(2, 2)),
@@ -16,40 +13,13 @@ from tensorflow.python.keras.layers import *
     layers.Flatten(),
     layers.Dense(256, activation='relu'),
     layers.Dense(10, activation='softmax')
-])'''
-
-
-
+])
 
 num_classes = 10
+
+model.compile(optimizer=tf.train.RMSPropOptimizer(0.001, decay=1e-6), loss='categorical_crossentropy', metrics=['accuracy'])
+
 (trainX, trainY), (testX, testY) = tf.keras.datasets.cifar10.load_data()
-
-
-model = tf.keras.models.Sequential()
-model.add(Conv2D(32, (3, 3), padding='same',
-                 input_shape=trainX.shape[1:]))
-model.add(Activation('relu'))
-model.add(Conv2D(32, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-
-model.add(Conv2D(64, (3, 3), padding='same'))
-model.add(Activation('relu'))
-model.add(Conv2D(64, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-
-model.add(Flatten())
-model.add(Dense(512))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-model.add(Dense(num_classes))
-model.add(Activation('softmax'))
-
-
-model.compile(optimizer=tf.train.AdamOptimizer(0.001), loss='categorical_crossentropy', metrics=['accuracy'])
 
 trainY = tf.keras.utils.to_categorical(trainY, num_classes=num_classes)
 testY = tf.keras.utils.to_categorical(testY, num_classes=num_classes)
